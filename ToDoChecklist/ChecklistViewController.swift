4b6682b8897462ef39330318ad0ae1cd28d12d71
Claudia Carrillo <ccarrillo@nearsoft.com>
2018-01-16 12:13:42 -0600

    Add ChecklistViewController

---
+++ ToDoChecklist/ChecklistViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ChecklistViewController.swift
+//  ToDoChecklist
+//
+//  Created by Caludia Carrillo on 1/16/18.
+//  Copyright © 2018 Claudia Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
+// MARK: - TableView Data Source methods
+extension ChecklistViewController {
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 60
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+    return cell
+  }
+}
+
+// MARK: - TableView Delegate methods
+extension ChecklistViewController {
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    guard let cell = tableView.cellForRow(at: indexPath) else {
+      return
+    }
+    
+    if cell.accessoryType == .none {
+      cell.accessoryType = .checkmark
+    }
+    else {
+      cell.accessoryType = .none
+    }
+    
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
+}
