6c19844f07c88d5c5ce8526a5fe3c2fa1d93b6c8
Claudia Carrillo <ccarrillo@nearsoft.com>
2018-01-21 00:03:50 -0600

    Refactor controllers

--- ToDoChecklist/Controllers/AllListsViewController.swift
+++ ToDoChecklist/Controllers/AllListsViewController.swift
@@ -1,40 +1,50 @@
 //
-//  AllListsViewController.swift
+//  ChecklistViewController.swift
 //  ToDoChecklist
 //
-//  Created by Caludia Carrillo on 1/20/18.
+//  Created by Caludia Carrillo on 1/16/18.
 //  Copyright © 2018 Claudia Carrillo. All rights reserved.
 //
 
 import UIKit
 
 class AllListsViewController: UITableViewController {
-  func makeCell(for tableView: UITableView) -> UITableViewCell {
-    let cellIdentifier = "Cell"
-    if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) {
-      return cell
-    } else {
-      return UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
-    }
+  private var lists = [Checklist]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.register(UINib(nibName: "ChecklistItemCell", bundle: nil), forCellReuseIdentifier: "ChecklistItemCell")
+    let checklist = Checklist()
+    checklist.name = "Checklist 1"
+    lists.append(checklist)
   }
 }
 
-// MARK: - Table Delegate Methods
-extension AllListsViewController {
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    performSegue(withIdentifier: "ShowChecklist", sender: nil)
-  }
-}
-
-// MARK: - Table Data Source Methods
+// MARK: - TableView Data Source methods
 extension AllListsViewController {
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 3
+    return lists.count
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = makeCell(for: tableView)
-    cell.textLabel!.text = "List \(indexPath.row)"
-    return cell
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItemCell") as? ChecklistItemCell
+    let checklist = lists[indexPath.row]
+    
+    if let checklistCell = cell {
+      checklistCell.itemNameLabel.text = checklist.name
+      return checklistCell
+    }
+    
+    return cell!
+  }
+}
+
+// MARK: - TableView Delegate methods
+extension AllListsViewController {
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    lists.remove(at: indexPath.row)
+    
+    let indexPaths = [indexPath]
+    tableView.deleteRows(at: indexPaths, with: .automatic)
   }
 }
